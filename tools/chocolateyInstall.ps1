dc8f72fa0674040cafe29e0ffbc7e0c1ba5bb2e5
jden <jason@denizac.org>
2015-04-11 11:54:39 -0700

    v 1.6.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'io.js'
 $fileType = 'msi'
-$url32 = 'https://iojs.org/dist/v1.6.2/iojs-v1.6.2-x86.msi'
-$url64 = 'https://iojs.org/dist/v1.6.2/iojs-v1.6.2-x64.msi'
+$url32 = 'https://iojs.org/dist/v1.6.4/iojs-v1.6.4-x86.msi'
+$url64 = 'https://iojs.org/dist/v1.6.4/iojs-v1.6.4-x64.msi'
 $silentArgs = '/quiet'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url32" "$url64"
